40b5c88c6c143ca7c1c465e9908a36c0cc425bad
jon-pruessmeier <pruessmeierjon@gmail.com>
2022-03-07 22:35:04 +0100

    changed routes for production

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ console.log(jsonData.length);
 
 
 
-app.get('/wikipages_project/api/wikipages/articles', (req, res) => {
+app.get('/api/wikipages/articles', (req, res) => {
     if (jsonData){
         res.status(200).send(JSON.stringify(jsonData));
     } else {
@@ -33,7 +33,7 @@ app.get('/wikipages_project/api/wikipages/articles', (req, res) => {
     }
 })
 
-app.get('/wikipages_project', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(indexHTML);
 });
 
